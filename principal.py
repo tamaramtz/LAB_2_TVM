4a325f4cd5022cb33db51026118ebe7a2a14dd30
tamaramtz <tamaramtz98@gmail.com>
2020-02-24 08:51:44 -0600

    Commit principal

---
+++ principal.py
@@ -0,0 +1,6 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: Tamara Mtz.
+# -- repositorio: https://github.com/tamaramtz/LAB_2_TVM.git
+# -- ------------------------------------------------------------------------------------ -- #

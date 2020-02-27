58f87633a9d7bcf8d06005e93f38cfe404671b7b
tamaramtz <tamaramtz98@gmail.com>
2020-02-27 07:57:46 -0600

    2nd commit

---
+++ funciones.py
@@ -0,0 +1,38 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: Tamara Mtz.
+# -- repositorio: https://github.com/tamaramtz/LAB_2_TVM.git
+# -- ------------------------------------------------------------------------------------ -- #
+import pandas as pd
+
+# -- ------------------------------------------------------------------------------------ -- #
+
+def f_leer_archivo(param_archivo):
+    '''
+
+    Parameters
+    ----------
+    param_archivo: str : nombre de archivo a leer
+    ----------
+    df_data : pd.DataFrame : con infomación obtenida e
+    Returns
+    -------
+    Debugging
+    ----------
+    param_archivo = 'archivo_tradeview_1.xlsx'
+
+    '''
+
+    # Leer archivo de datos y guardarlo en un DataFrame
+    df_data = pd.read_excel('archivos/' + param_archivo, sheet_name='Hoja1')
+
+    # Cnvertir en minusculas el nombre de las columnas
+    df_data.columns = [list(df_data.columns)[0].lower()
+                       for i in range(0, len(df_data.columns))]
+    # Asegurar que ciertas son del tipo numerico
+    numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size', 'swap',
+               'taxes', 'other']
+    df_data(numcols) = df_data[numcols].apply(pd.to_numeric)
+
+    return df_data
--- principal.py
+++ principal.py
@@ -1,6 +1,10 @@
 # -- ------------------------------------------------------------------------------------ -- #
-# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
-# -- archivo: datos.py - datos generales para uso en el proyecto
-# -- mantiene: Tamara Mtz.
-# -- repositorio: https://github.com/tamaramtz/LAB_2_TVM.git
-# -- ------------------------------------------------------------------------------------ -- #
+# # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# # -- archivo: datos.py - datos generales para uso en el proyecto
+# # -- mantiene: Tamara Mtz.
+# # -- repositorio: https://github.com/tamaramtz/LAB_2_TVM.git
+# # -- ------------------------------------------------------------------------------------ -- #
+
+import funciones as fn
+
+datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')

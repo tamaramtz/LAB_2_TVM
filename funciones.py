a373376f51d89e15b9eed338dea0f8cf200f1701
tamaramtz <tamaramtz98@gmail.com>
2020-02-27 08:53:17 -0600

    2nd commit

--- funciones.py
+++ funciones.py
@@ -13,13 +13,12 @@ def f_leer_archivo(param_archivo):
 
     Parameters
     ----------
-    param_archivo: str : nombre de archivo a leer
-    ----------
-    df_data : pd.DataFrame : con infomación obtenida e
+    param_archivo : str : nombre de archivo a leer
     Returns
     -------
+    df_data : pd.DataFrame : con informacion contenida en archivo leido
     Debugging
-    ----------
+    ---------
     param_archivo = 'archivo_tradeview_1.xlsx'
 
     '''
@@ -36,5 +35,39 @@ def f_leer_archivo(param_archivo):
 
     df_data[numcols] = df_data[numcols].apply(pd.to_numeric)
 
-    print(df_data)
+    #print(df_data)
     return df_data
+# -- ------------------------------------------------------ FUNCION: Pips por instrumento -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# -- calcular el tamaño de los pips por instrumento
+
+def f_pip_size(param_ins):
+    """
+       Parameters
+       ----------
+       param_ins : str : nombre de instrumento
+       Returns
+       -------
+       Debugging
+       -------
+       param_ins = 'usdjpy'
+       """
+
+    # encontrar y eliminar un guion bajo
+    # inst = param_ins.replace('_', '')
+
+    # transformar a minusculas
+    inst = param_ins.lower()
+
+    # lista de pips por instrumento
+    pips_inst = {'usdjpy': 100, 'gbpjpy': 100, 'eurjpy': 100, 'cadjpy': 100,
+                 'chfjpy': 100,
+                 'eurusd': 10000, 'gbpusd': 10000, 'usdcad': 10000, 'usdmxn': 10000,
+                 'audusd': 10000, 'nzdusd': 10000,
+                 'usdchf': 10000,
+                 'eurgbp': 10000, 'eurchf': 10000, 'eurnzd': 10000, 'euraud': 10000,
+                 'gbpnzd': 10000, 'gbpchf': 10000, 'gbpaud': 10000,
+                 'audnzd': 10000, 'nzdcad': 10000, 'audcad': 10000,
+                 'xauusd': 10, 'xagusd': 10, 'btcusd': 1}
+
+    return pips_inst[inst]
--- principal.py
+++ principal.py
@@ -8,3 +8,4 @@
 import funciones as fn
 
 datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
+fn.f_pip_size(param_ins='usdjpy')
